e352fc9c2513b60b89567983fb1190807ce6306e
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2026-06-02 22:31:42 +0600

    info details done

--- scripte/index.js
+++ scripte/index.js
@@ -1,3 +1,8 @@
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join("");
+};
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json()) //promise of json data
@@ -24,6 +29,80 @@ const loadLevelWord = (id) => {
       clickBtn.classList.add("active");
       displayLevelWord(data.data);
     });
+};
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  const detailsBox = document.getElementById("details-container");
+
+  detailsBox.innerHTML = `
+    <div class="border border-slate-200 rounded-xl p-6">
+
+      <!-- Word -->
+      <h2 class="text-3xl font-bold text-black mb-8">
+        ${word.word}
+        <span class="text-2xl font-medium">
+          (<i class="fa-solid fa-microphone-lines"></i>
+          : ${word.pronunciation || "N/A"})
+        </span>
+      </h2>
+
+      <!-- Meaning -->
+      <div class="mb-8">
+        <h3 class="text-2xl font-bold mb-3">
+          Meaning
+        </h3>
+        <p class="text-xl text-gray-700">
+          ${word.meaning || "Meaning not available"}
+        </p>
+      </div>
+
+      <!-- Example -->
+      <div class="mb-8">
+        <h3 class="text-2xl font-bold mb-3">
+          Example
+        </h3>
+        <p class="text-xl text-gray-700">
+          ${word.example || "Example not available"}
+        </p>
+      </div>
+
+      <!-- Synonyms -->
+      <div>
+        <span
+          class="inline-block border-2 border-sky-500 px-2 py-1 text-lg font-semibold"
+        >
+          সমার্থক শব্দ গুলো
+        </span>
+
+        <div class="flex flex-wrap gap-3 mt-4">
+          ${
+            word.synonyms?.length
+              ? word.synonyms
+                  .map(
+                    (syn) => `
+                    <span
+                      class="bg-slate-100 border border-slate-200 px-4 py-2 rounded-md text-lg"
+                    >
+                      ${syn}
+                    </span>
+                  `,
+                  )
+                  .join("")
+              : `<span class="text-gray-500">No synonyms available</span>`
+          }
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.getElementById("word_modal").showModal();
 };
 
 const displayLevelWord = (words) => {
@@ -55,7 +134,7 @@ const displayLevelWord = (words) => {
         <p class="text-gray-600" font-semibold>Meaning /Pronounciation</p>
         <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "meaning is not available"} / ${word.pronunciation ? word.pronunciation : "pronunciation is not available"}"</div>
         <div class="flex justify-between items-center">
-          <button onClick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
+          <button onClick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
             <i class="fa-solid fa-circle-info"></i>
           </button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
